c41516c69f5ba37cf9fafc54bb9e9aecb891428d
Greg Pedder <greg.pedder@glasgow.ac.uk>
2026-04-09 11:16:14 +0100

    Plugin update mod_hvp. Cloned from GitHub repo as STABLE branch is using submodules.

--- public/mod/hvp/classes/curl.php
+++ public/mod/hvp/classes/curl.php
@@ -50,7 +50,7 @@ class curl extends moodlecurl {
     /**
      * @inheritdoc
      */
-    public function put($url, $params = '', $options = array(), $includeuserpassword = true) {
+    public function put($url, $params = '', $options = array()) {
         $options['CURLOPT_CUSTOMREQUEST'] = 'PUT';
         $options['CURLOPT_POSTFIELDS'] = $params;
         return $this->request($url, $options);
