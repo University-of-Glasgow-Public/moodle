f24e838d584e6d1af2a7ed8f3f8cbcbdab42b3c7
Ferenc-Frank-Lengyel <ferenc.lengyel@glasgow.ac.uk>
2026-06-30 11:19:22 +0100

    MGU-1455: Removing performance tracking lines

--- local/ugassessment/refresh.php
+++ local/ugassessment/refresh.php
@@ -48,22 +48,12 @@ if ($confirm) {
     \core\notification::add(get_string('refreshsnapshot', 'local_ugassessment'), \core\output\notification::NOTIFY_INFO);
 
     $start = microtime(true);
-    $startmemory = memory_get_usage(true);
-    $peakstart = memory_get_peak_usage(true);
-    $startqueries = $DB->perf_get_queries();
 
     $result = \local_ugassessment\snapshot_builder::rebuild_snapshot(false);
 
     $duration = microtime(true) - $start;
-    $endmemory = memory_get_usage(true);
-    $peakend = memory_get_peak_usage(true);
-    $endqueries = $DB->perf_get_queries();
 
     mtrace('Snapshot rebuild completed in ' . round($duration, 2) . ' seconds.');
-    mtrace(' - Memory start: ' . round($startmemory / 1024 / 1024, 2) . ' MB');
-    mtrace(' - Memory end: ' . round($endmemory / 1024 / 1024, 2) . ' MB');
-    mtrace(' - Peak memory: ' . round($peakend / 1024 / 1024, 2) . ' MB');
-    mtrace(' - DB queries: ' . ($endqueries - $startqueries));
 
     echo $OUTPUT->notification(get_string('snapshotrefreshsuccess', 'local_ugassessment'), 'notifysuccess');
 
